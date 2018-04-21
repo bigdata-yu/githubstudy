9ba8022836402f93c730b654cd6f85630666dfb1
bigdata-yu <1282496490@qq.com>
2018-04-21 17:57:24 +0800

    新建项目

---
+++ src/Demo.java
@@ -0,0 +1,14 @@
+/**
+ * Description：xxx<br/>
+ * Copyright (c) ， 2018， xiangchaoyu <br/>
+ * This program is protected by copyright laws. <br/>
+ * Date：2018年04月21日
+ *
+ * @author 项超羽
+ * @version : 1.0
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hellolaotie");
+    }
+}
